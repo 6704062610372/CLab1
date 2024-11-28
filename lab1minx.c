1f25803554afe01e41ad518e5dc714612d719f6c
6704062610372 <s6704062610372@email.kmutnb.ac.th>
2024-11-28 15:08:08 +0700

    same line input number

--- lab1minx.c
+++ lab1minx.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main(){
-	int var1;
-	int var2;
+	int var1,var2;
 	int sum;
-	scanf("%d",&var1);
+	scanf("%d",&var1); 
 	scanf("%d",&var2);
 	if (var1 > var2){
 		sum = var1 - var2;
