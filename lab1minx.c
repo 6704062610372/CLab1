4872559cbe065f5abb9a21f5461de90972cabc91
6704062610372 <s6704062610372@email.kmutnb.ac.th>
2024-11-28 14:49:17 +0700

    input int if else

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
-	int num1=5;
-	int num2=10;
-	if (num1>num2){
-		printf("num1 greater than num2");
-	} 
+	int n1;
+	int n2;
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+	if (n1 > n2){
+		printf("n1 is more n2");
+	}
 	else {
-		printf("num2 greater than num1");
+		printf("n2 is more n1");
 	}
 }
